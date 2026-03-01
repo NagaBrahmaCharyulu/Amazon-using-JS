71f330e45409ec3eb2cd9c4688e10055b121802e
NagaBrahmaCharyulu <pandubethapudi05@gmail.com>
2026-03-01 12:40:38 +0530

    Delete products from cart when clicking delete

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,7 +28,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select id="js-product-quantity">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
